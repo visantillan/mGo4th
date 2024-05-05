8acd25a76d5c02d12f51975dcbacf83c8767aff5
visantillan <viktor.santillan@gmail.com>
2024-05-05 16:59:45 -0600

    leetcode ex

---
+++ leetcode/mergeStringsAlt.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println(mergeAlternately("ab", "cdefg"))
+
+	fmt.Println(gcdOfStrings("ABABAB", "AB"))
+}
+
+func gcdOfStrings(str1 string, str2 string) string {
+
+	return str1 + str2
+}
+
+func mergeAlternately(word1, word2 string) string {
+
+	var altWord string
+
+	if len(word1) == 0 {
+		return "word1 should be at least 1 character"
+	}
+
+	sWord := getShortWord(word1, word2)
+
+	for i := 0; i <= len(sWord)-1; i++ {
+		altWord += string(word1[i]) + string(word2[i])
+	}
+
+	return altWord + getBigWord(word1, word2)[len(sWord):]
+}
+
+func getShortWord(word1, word2 string) string {
+	if len(word1) > len(word2) {
+		return word2
+	}
+
+	return word1
+}
+
+func getBigWord(word1, word2 string) string {
+	if len(word1) > len(word2) {
+		return word1
+	}
+
+	return word2
+}
