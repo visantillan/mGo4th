b8a87948b9506f9181915c90c8478f61725a94ee
visantillan <viktor.santillan@gmail.com>
2024-05-28 13:48:27 -0600

    Update mergeStringsAlt.go

--- leetcode/mergeStringsAlt.go
+++ leetcode/mergeStringsAlt.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println(mergeAlternately("ab", "cdefg"))
-
-	fmt.Println(gcdOfStrings("ABABAB", "AB"))
+	fmt.Println(mergeAlternately("ab", "cdefg"))
 }
 
-func gcdOfStrings(str1 string, str2 string) string {
+func mergeAlternately2(word1, word2 string) string {
 
-	return str1 + str2
+	var altWord string
+
+	if len(word1) == 0 {
+		return "word1 should be at least 1 character"
+	}
+
+	sWord := getShortWord(word1, word2)
+
+	for i := 0; i <= len(sWord)-1; i++ {
+		altWord += string(word1[i]) + string(word2[i])
+	}
+
+	return altWord + getBigWord(word1, word2)[len(sWord):]
 }
 
 func mergeAlternately(word1, word2 string) string {
